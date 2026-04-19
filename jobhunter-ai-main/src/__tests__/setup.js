965152b18145b3a48580542d7c4092244c82b72b
MayuriJ185 <maayurijadhav18@gmail.com>
2026-04-19 17:09:54 -0400

    wrong push

--- jobhunter-ai-main/src/__tests__/setup.js
+++
@@ -1,5 +0,0 @@
-import '@testing-library/jest-dom'
-import { beforeEach } from 'vitest'
-
-// Clear URL hash between tests so hash-based routing doesn't leak state
-beforeEach(() => { window.location.hash = '' })
